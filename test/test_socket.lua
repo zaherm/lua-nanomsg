a8adbad2cbcc51547fc3a619400f391efa145317
zaherm <zaherm@gmail.com>
2016-11-15 09:40:30 +0000

    changed socket test to simulate server/client pair

--- test/test_socket.lua
+++ test/test_socket.lua
@@ -1,16 +1,28 @@
 local nanomsg = require("nanomsg")
-local socket = nanomsg.socket(nanomsg.AF_SP, nanomsg.REQ)
+local rcvtimeo = 100
 
-assert(socket)
-rcvtimeo = 100
-socket:setopt(nanomsg.SOL_SOCKET, nanomsg.RCVTIMEO, rcvtimeo)
-assert(socket:getopt(nanomsg.SOL_SOCKET, nanomsg.RCVTIMEO) == rcvtimeo)
-print("done")
+local server_socket = nanomsg.socket(nanomsg.AP_SP, nanomsg.PAIR)
+server_socket:setopt(nanomsg.SOL_SOCKET, nanomsg.RCVTIMEO, rcvtimeo)
+local server = nanomsg.bind(server_socket, "tcp://127.0.0.1:9094")
 
-local conn = nanomsg.connect(socket, "tcp://127.0.0.1:1234")
-assert(conn)
-conn:send("ping")
-print(conn:recv())
-conn:shutdown()
+local client_socket = nanomsg.socket(nanomsg.AP_SP, nanomsg.PAIR)
+client_socket:setopt(nanomsg.SOL_SOCKET, nanomsg.RCVTIMEO, rcvtimeo)
+local client = nanomsg.connect(client_socket, "tcp://127.0.0.1:9094")
+os.execute("sleep 1")
+client:send("hello from client")
+while true do
+    local server_msg = server:recv()
+    local client_msg = client:recv()
+    if server_msg then
+        print("server_msg:", server_msg)
+        server:send("hello from server")
+    end
+    if client_msg then
+        print("client_msg:", client_msg)
+        client:send("hello from client")
+    end
+    
+end
 
-
+server:shutdown()
+client:shutdown()
